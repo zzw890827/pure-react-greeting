d3af34aa3ed01e545c8bd3746e824c2851e7571f
Lee Wenqian <zzw890827@gmail.com>
2018-08-10 11:31:44 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Greeting() {
+  let username = 'null';
+  return (
+    <span>
+      {(username === 'undefined' || username === 'null') ?
+        'Not logged in.' : 'Hello ' + username}
+    </span>
+  )
+}
+
+ReactDOM.render(<Greeting/>, document.getElementById('root'));
